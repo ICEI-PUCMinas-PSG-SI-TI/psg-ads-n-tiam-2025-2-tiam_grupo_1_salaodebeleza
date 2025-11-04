a5562551c3d155781f5acdd447a94c239b66b770
Lucas Phillipe Freitas Dias <lucas.pfd.2002@gmail.com>
2025-11-04 20:04:28 -0300

    Corrigindo app.js

--- src/AgendaGlow/App.js
+++ src/AgendaGlow/App.js
@@ -1,9 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthProvider } from './src/context/AuthContext';
 import AppRoutes from './src/navigations/AppRoutes';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
